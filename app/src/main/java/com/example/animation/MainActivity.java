d41757a600b00160e7fee3dc6f906279f85b0f42
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-03-22 15:51:20 +0100

    Animation for continuous spinning

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -7,15 +7,40 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int tracker = 1;
+    public float angle = 180f;
+    public float tracker()
+    {
+        float simple = 180f;
+        if (tracker <= 1){
+            tracker++;
+            return simple;
+        }
+        else
+        {
+            angle += 180f;
+            return angle;
+        }
+    }
     public void fade(View view){
+        //ImageView full = findViewById(R.id.full);
+        //full.animate().alpha(1f).setDuration(2000);
         ImageView small = findViewById(R.id.small);
-        ImageView full = findViewById(R.id.full);
-        small.animate().alpha(0f).setDuration(2000);
-        full.animate().alpha(1f).setDuration(2000);
+        //small.animate().alpha(0f).setDuration(2000);
+        //small.animate().translationXBy(1000f).setDuration(2000);
+        small.animate().rotation(tracker()).setDuration(1000);
+
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        ImageView small = findViewById(R.id.small);
+        small.setTranslationX(-1000f);
+        small.animate().translationXBy(1000f).setDuration(2000);
+*/
+
     }
 }
